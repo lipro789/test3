138edf72f6d2260e1a354cce3789d892d9a70605
lipro789 <124849143+lipro789@users.noreply.github.com>
2023-06-17 21:41:37 +0800

    Update CClassAdd.cpp

--- CClassAdd.cpp
+++ CClassAdd.cpp
@@ -2,7 +2,7 @@
 #include "CClassAdd.h"
 //#include "MFC1StudentManager.h"//
 
-// CClassAdd �Ի���
+// CClassAdd 对话框
 
 IMPLEMENT_DYNAMIC(CClassAdd, CDialog)
 
@@ -32,16 +32,16 @@ END_MESSAGE_MAP()
 
 void CClassAdd::OnBnClickedOk()
 {
-	// TODO: �ڴ����ӿؼ�֪ͨ�����������
-	//��ͨ����������Ӧ�ؼ������˱���֮�󣬿���ͨ��UpdateData()������ȡ����¶Ի���ؼ���ֵ
-	UpdateData(TRUE);//�������ڻ�ȡ���뵽�ؼ�������,�����浽�����У��ؼ�->����
-	m_pConnection.CreateInstance(__uuidof(Connection));//ʵ�������Ӷ���
-	m_pConnection->ConnectionTimeout = 20;//�������Ӵ�ʱ��
+	// TODO: 在此添加控件通知处理程序代码
+	//在通过类向导向相应控件添加了变量之后，可以通过UpdateData()函数获取或跟新对话框控件的值
+	UpdateData(TRUE);//函数用于获取输入到控件的数据,并保存到变量中，控件->变量
+	m_pConnection.CreateInstance(__uuidof(Connection));//实例化连接对象
+	m_pConnection->ConnectionTimeout = 20;//设置连接打开时间
 	HRESULT hr;
-	hr=m_pConnection->Open("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=ѧ����Ϣ����ϵͳ.mdb", "", "", adConnectUnspecified);
+	hr=m_pConnection->Open("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=学生信息管理系统.mdb", "", "", adConnectUnspecified);
 	if (FAILED(hr))
 	{
-		AfxMessageBox(_T("��ʧ��"));
+		AfxMessageBox(_T("打开失败"));
 		return;
 	}
 	_RecordsetPtr pRecordset;
@@ -49,17 +49,17 @@ void CClassAdd::OnBnClickedOk()
 
 	try
 	{
-		//ִ��SQL��䣬�õ���¼��, connection�����Ѻ����ݿ�����
-		hr = pRecordset->Open("select * from �༶��Ϣ��", m_pConnection.GetInterfacePtr(), adOpenDynamic, adLockOptimistic, adCmdText);
+		//执行SQL语句，得到记录集, connection必须已和数据库连接
+		hr = pRecordset->Open("select * from 班级信息表", m_pConnection.GetInterfacePtr(), adOpenDynamic, adLockOptimistic, adCmdText);
 		if (SUCCEEDED(hr))
 		{
-			pRecordset->AddNew();///�����¼�¼
-			pRecordset->PutCollect("�༶����", _variant_t(m_ClassName));
-			pRecordset->PutCollect("�༶���", _variant_t(m_ClassNum));
-			pRecordset->PutCollect("�꼶", _variant_t(m_ClassGrade));
+			pRecordset->AddNew();///添加新记录
+			pRecordset->PutCollect("班级名称", _variant_t(m_ClassName));
+			pRecordset->PutCollect("班级编号", _variant_t(m_ClassNum));
+			pRecordset->PutCollect("年级", _variant_t(m_ClassGrade));
 			pRecordset->Update();
 			OnOK();
-			AfxMessageBox(_T("���Ӽ�¼�ɹ���"));
+			AfxMessageBox(_T("添加纪录成功！"));
 		}
 
 	}
@@ -69,7 +69,7 @@ void CClassAdd::OnBnClickedOk()
 		AfxMessageBox(e->ErrorMessage());
 		return;
 	}
-	//�������ڸ��¿ؼ��е����ݣ�����->�ؼ���
-	//��������˱�����ֵ���뽫�����µ��ؼ���ʾ�����ɵ��ô˺���
+	//函数用于更新控件中的数据，变量->控件，
+	//当你更改了变量的值并想将它更新到控件显示出来可调用此函数
 	UpdateData(FALSE);
 }
